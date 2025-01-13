79cd5a3cb089f5a7532b1b7c9dfb59aca2930a1f
EngCamo <learn3038it@gmail.com>
2025-01-14 06:39:10 +0800

    post api traiing

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -16,25 +16,35 @@ export async function GET() {
 export async function POST(req: Request) {
     await connectMongoDB();
     try {
-        const requestdData = await req.json();
-        console.log('here the sent data :' + requestdData);
 
-        const { title, content } = await req.json();
+        const { title, content } =  await req.json();
         if (!title || !content) {
-            return NextResponse.json({ message: 'Title and content are required',error: error.message }, { status: 400 })
-
+            return NextResponse.json(
+                { message: "Title and content are required" },
+                { status: 400 }
+            );
         }
 
         const newPost = new Posts({
             title,
-            content
-        })
+            content,
+        });
 
+       
         await newPost.save();
-        return NextResponse.json({ message: 'Post created successfully!', posts: newPost }, { status: 201 })
+
+        return NextResponse.json(
+            { message: "Post created successfully!", posts: newPost },
+            { status: 201 }
+        );
     } catch (error) {
-        return NextResponse.json({ message: "Failed to create post", error: error }, { status: 500 });
+        
+
+
+        console.error("Error creating post: ", error);
+        return NextResponse.json(
+            { message: "Failed to create post", error: error.message },
+            { status: 500 }
+        );
     }
-
-
 }
